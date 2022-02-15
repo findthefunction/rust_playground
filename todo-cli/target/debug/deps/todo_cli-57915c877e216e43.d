77c3f2371ecf593b8b98e204517952f97de3b92c
findthefunction <findthefunction@gmail.com>
2022-02-15 12:09:18 -0800

    Rust playground

---
+++ todo-cli/target/debug/deps/todo_cli-57915c877e216e43.d
@@ -0,0 +1,5 @@
+/home/onizuka/playground/rust/todo-cli/target/debug/deps/todo_cli-57915c877e216e43: src/main.rs
+
+/home/onizuka/playground/rust/todo-cli/target/debug/deps/todo_cli-57915c877e216e43.d: src/main.rs
+
+src/main.rs:
---
+++ todo-cli/target/debug/todo-cli.d
@@ -0,0 +1,1 @@
+/home/onizuka/playground/rust/todo-cli/target/debug/todo-cli: /home/onizuka/playground/rust/todo-cli/src/main.rs
